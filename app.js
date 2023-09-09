5eb9b0ac795e9994b485a933aa9ca67fa63d9432
MohdAnasQureshi <qureshianas471@gmail.com>
2023-09-10 00:44:48 +0530

    port change

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const logger = require("morgan");
 const cors = require("cors");
 
 const app = express();
-
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
@@ -15,4 +15,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/v1", require("./src/v1/routes"));
 
+app.listen(port, () => {
+  console.log("server on port" + port);
+});
 module.exports = app;
